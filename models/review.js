ce1f2d986438e4789a325f8404712efca040d40f
shu-hu <shunuan.hu69@qmail.cuny.edu>
2021-07-19 23:32:17 -0400

    finish style favorite page

--- models/review.js
+++ models/review.js
@@ -5,7 +5,7 @@ export {
 }
 const reviewSchema = new mongoose.Schema(
     {
-    gameId: Number,
+    gameId: String,
     content: String,
     rating: {type: Number, min: 1, max: 5, default: 5},
     user: {type: mongoose.Schema.Types.ObjectId, ref: 'User'}
